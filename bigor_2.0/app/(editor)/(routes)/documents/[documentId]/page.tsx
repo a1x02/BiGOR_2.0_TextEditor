ad884936d682ef505a8d386eb2dcc10d1a829760
a1x02 <Sashurik2002@gmail.com>
2024-04-07 18:51:42 +0300

    feat: Реализовать добавление иконки

--- bigor_2.0/app/(editor)/(routes)/documents/[documentId]/page.tsx
+++ bigor_2.0/app/(editor)/(routes)/documents/[documentId]/page.tsx
@@ -1,6 +1,40 @@
-const documentIdPage = () => {
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { Toolbar } from "@/components/Toolbar";
+
+interface DocumentIdPageProps {
+    params: {
+        documentId: Id<"documents">;
+    }
+}
+
+const documentIdPage = ({
+    params
+}: DocumentIdPageProps) => {
+    const document = useQuery(api.documents.getById, {
+        documentId: params.documentId
+    });
+
+    if (document === undefined) {
+        return(<div>Загрузка...</div>)
+    }
+
+    if (document === null) {
+        return(<div>
+            Ничего не нашлось
+        </div>)
+    }
+
     return ( 
-        <div>DocumentId</div>
+        <div className="pb-40">
+            <div className="h-[35vh]" />
+            <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+                <Toolbar initialData={document}/>
+            </div>
+        </div>
      );
 }
  
